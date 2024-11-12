62cc3da8648ee63c3b2d289b9c36fc3afdc5c6b1
mbjin <90257022+NewBst99@users.noreply.github.com>
2024-11-12 20:31:23 +0900

    1112(Port forwarding, Server binding)

--- src/main/java/gameservice/StartServer.java
+++ src/main/java/gameservice/StartServer.java
@@ -4,15 +4,22 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import java.io.IOException;
 
+import java.net.InetAddress;
+
 public class StartServer {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        String host = "Local IPv4";
+        int port = 9090;
+
         Server server = ServerBuilder.forPort(9090)
                 .addService(new StubServer())
                 .build();
 
+        InetAddress inetAddress = InetAddress.getByName(host);
+        System.out.println("Server started at " + inetAddress.getHostAddress() + ":" + port);
+
         server.start();
-        System.out.println("Server started at " + server.getPort());
         server.awaitTermination();
     }
 }
